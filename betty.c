da3dba82614e4c04d319b71ce85729019e0e0453
mikewedam <michael.abocco@gmail.com>
2023-12-14 00:37:16 -0800

    update

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; < 10; i++)
+	{
+		printf("Hello, Betty!\n");
+	}
+
+	return (0);
+}
